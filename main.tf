4be3a25f2361e9d570b340dadcb34bcd57304664
peterchiviyski <peter.chiviyski@hashicorp.com>
2022-12-12 13:38:39 +0200

    random-pet

--- main.tf
+++ main.tf
@@ -1,15 +1,10 @@
-variable "length" {
-  description = "Specify the length of the sample resource."
-}
+# The following example shows how to generate a unique pet name
+# for an AWS EC2 instance that changes each time a new AMI id is
+# selected.
 
-resource "random_string" "sample" {
-  length = var.length
-}
-
-output "test" {
-  value = random_string.sample.id
-}
-
-output "output" {
-  value = random_string.sample.id
+resource "random_pet" "server" {
+  keepers = {
+    # Generate a new pet name each time we switch to a new AMI id
+    ami_id = var.ami_id
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "ami_id" {
+    type = string
+    default = "PETNAMEHERE"
+
+}
